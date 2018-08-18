0cbe674aaf373c29b8fd1c13ba10c3bf3072bbe5
JoesMac <jantongiovanni@csumb.edu>
2018-08-18 11:32:55 -0700

    Realm object class

---
+++ Pick Up Lines Proto/Line.swift
@@ -0,0 +1,12 @@
+//
+//  Line.swift
+//  Pick Up Lines
+//
+//  Created by JoesMac on 8/18/18.
+//  Copyright © 2018 Kilo Loco. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+
---
+++ Pick Up Lines Proto/PickUpLine.swift
@@ -0,0 +1,27 @@
+//
+//  PickUpLine.swift
+//  Pick Up Lines
+//
+//  Created by JoesMac on 8/18/18.
+//  Copyright © 2018 Kilo Loco. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+@objcMembers class PickUpLine: Object {
+    
+    dynamic var line: String = ""
+    let score = RealmOptional<Int>()
+    dynamic var email: String? = nil
+
+    convenience init(line: String, score: Int?, email: String?){
+        self.init()
+        self.line = line
+        self.score.value = score
+        self.email = email
+    }
+
+
+
+}
