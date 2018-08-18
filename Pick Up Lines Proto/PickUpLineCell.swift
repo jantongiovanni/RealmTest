05a9b72cadb42947fba4d50dd0bec388db90409c
JoesMac <jantongiovanni@csumb.edu>
2018-08-18 11:05:01 -0700

    first commit

---
+++ Pick Up Lines Proto/PickUpLineCell.swift
@@ -0,0 +1,23 @@
+//
+//  PickUpLineCell.swift
+//  Pick Up Lines Proto
+//
+//  Created by Kyle Lee on 10/14/17.
+//  Copyright © 2017 Kilo Loco. All rights reserved.
+//
+
+import UIKit
+
+class PickUpLineCell: UITableViewCell {
+
+    @IBOutlet weak var lineLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    func configure(with pickUpLine: Any) {
+        lineLabel.text = ""
+        scoreLabel.text = ""
+        emailLabel.text = ""
+    }
+
+}
---
+++ Pick Up Lines Proto/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Pick Up Lines Proto
+//
+//  Created by Kyle Lee on 10/14/17.
+//  Copyright © 2017 Kilo Loco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func onAddTapped() {
+        AlertService.addAlert(in: self) { (line, score, email) in
+            print(line, score, email)
+        }
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PickUpLineCell") as? PickUpLineCell else { return UITableViewCell() }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 63
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected")
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        print("delete")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
